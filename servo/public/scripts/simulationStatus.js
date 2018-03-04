78ad6ee271a465143f519b0bc24e818e686df630
SANJAI <sanreaper@gmail.com>
2018-03-04 23:39:21 +0530

    webUI added

--- servo/public/scripts/simulationStatus.js
+++ servo/public/scripts/simulationStatus.js
@@ -1,4 +1,9 @@
 function getAllInfo(){
+	var user1=document.getElementById("user");
+	var type1=document.getElementById("type");
+	var loc1=document.getElementById("loc");
+	var pos1=document.getElementById("pos");
+	var status1=document.getElementById("status");
 	var xhr = new XMLHttpRequest()
 	xhr.open('GET','/sim/getAllInfo',true);
 	xhr.send();
@@ -7,6 +12,39 @@ function getAllInfo(){
 			if(xhr.status == 200){
 				var res =xhr.responseText;	//the returned value
 				console.log(JSON.parse(res));	//parse the JSON value (sent with JSON.stringify())
+				var dataJ=JSON.parse(res);
+				var tbl = document.getElementById("resTbl");
+				var tblBody = document.createElement("tbody");
+				for(var i=0;i<dataJ.length;i++){
+		
+					var row = document.createElement("tr");
+					var cell=document.createElement("td");
+					user1= document.createTextNode(dataJ[i].uid );
+					cell.appendChild(user1);
+					row.appendChild(cell);
+					var cell=document.createElement("td");
+					type1= document.createTextNode(dataJ[i].type);
+					cell.appendChild(type1);
+					row.appendChild(cell);
+					var cell=document.createElement("td");
+					position1= document.createTextNode(dataJ[i].position);
+					cell.appendChild(position1);
+					row.appendChild(cell);
+					var cell=document.createElement("td");
+					status1= document.createTextNode(dataJ[i].status);
+					cell.appendChild(status1);
+					row.appendChild(cell);
+					var cell=document.createElement("td");
+					location1= document.createTextNode(dataJ[i].location);
+					cell.appendChild(location1);
+					row.appendChild(cell);
+			
+					
+					tblBody.appendChild(row);
+					
+					}
+					
+				tbl.appendChild(tblBody);
 			}
 			else{
 				console.log("Some Error");
@@ -16,3 +54,7 @@ function getAllInfo(){
 }
 
 window.onload=getAllInfo;
+
+
+
+
