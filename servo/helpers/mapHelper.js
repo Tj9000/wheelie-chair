97ccb5f8f2b8d4f5e46b46384ef69c630b8e0b41
Tj9000 <tejas050197@gmail.com>
2018-04-21 07:36:51 +0530

    removed logs

--- servo/helpers/mapHelper.js
+++ servo/helpers/mapHelper.js
@@ -28,19 +28,15 @@ const dirResolver={
 function getPath(floor,from,to){
 	var Graph = require('node-dijkstra')
 	var route = new Graph(require('../maps/'+floor+'_map'))
-	// console.log(route.path(from,to))
 	return route.path(from,to)
 }
 
 function getDirections(floor,from,to){
-	// var Graph = require('node-dijkstra')
 	var p = getPath(floor,from,to)
 	const plan=require('../maps/'+floor+'_plan')
 	const map=require('../maps/'+floor+'_map')
 	L=[]
 	for (var i = 0; i < p.length-1; i++) {
-		// console.log(plan[p[i]][p[i+1]])
-		// console.log(map[p[i]][p[i+1]])
 		L.push([
 			plan[p[i]][p[i+1]],
 			map[p[i]][p[i+1]]
@@ -51,9 +47,6 @@ function getDirections(floor,from,to){
 	let prev= L[0][0];
 	L[0][0]='F'
 	for (var i = 1; i < L.length; i++) {
-		// console.log(plan[p[i]][p[i+1]])
-		// console.log(map[p[i]][p[i+1]])
-		let next = dirResolver[prev][L[i][0]];
 		prev = L[i][0]
 		L[i][0]=next;
 
